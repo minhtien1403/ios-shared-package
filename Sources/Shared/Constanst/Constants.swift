f5f08ab0d93f2465c248e7aeeeebe4c00ef4d91b
partnertientm2 <partnertientm2@abbank.vn>
2025-11-04 11:16:55 +0700

    add constanst file

---
+++ Sources/Shared/Constanst/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Shared
+//
+//  Created by partnertientm2 on 4/11/25.
+//
+
+import Foundation
+
+struct URLs {
+        
+    struct Git {
+        static let base = "https://api.github.com"
+    }
+}
