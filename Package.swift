fad5f00f6d4dacb9d4392c25411b58b7777bf613
partnertientm2 <partnertientm2@abbank.vn>
2025-11-04 15:40:58 +0700

    add snapKit dependency

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,10 +14,15 @@ let package = Package(
             targets: ["Shared"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.6.0")
+    ],
     targets: [
         .target(
             name: "Shared",
-            dependencies: [],
+            dependencies: [
+                "SnapKit"
+            ],
             path: "Sources"
         ),
         .testTarget(
@@ -26,4 +31,3 @@ let package = Package(
         ),
     ]
 )
-
