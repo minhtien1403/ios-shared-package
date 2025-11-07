b917a6ba44341736032e10f44280aab8cf93fe5e
partnertientm2 <partnertientm2@abbank.vn>
2025-11-07 09:51:03 +0700

    add dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.6.0")
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.6.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0")
     ],
     targets: [
         .target(
